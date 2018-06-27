efc8fbea89a53f1d99b201529a2455ef1b226da3
ajah007 <40643567+ajah007@users.noreply.github.com>
2018-06-27 14:30:13 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-    consumer_key:'OiAJw5KAB3W9xbM8adCSDjhPZ',
-    consumer_secret:'yRZPGrG619yudsszsBJUdTuXzWQlfVTzwIjHK7hvbYJo2TqfDK',
-    access_token_key:'2842934894-OyDZx81xokbmOBP8zCbuOwpRoS61UT3LPLlzehS',
-    access_token_secret:'ZtfrG1mZQREp4oTal8RPARFOAbli9ASZZZ8beM86xtttB'
-}
